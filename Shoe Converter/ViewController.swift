3297545ace0fd4e382467433ab6b88f22b155d1e
Arno Smit <appel268576@gmail.com>
2015-01-05 08:42:11 +0100

    Initial Commit

---
+++ Shoe Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shoe Converter
+//
+//  Created by Arno Smit on 05/01/15.
+//  Copyright (c) 2015 Namib Lost in Sweden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
